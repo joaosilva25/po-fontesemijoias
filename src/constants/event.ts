5a79d216f3bb1ebb4c69f436f6438944159d0c28
João Silva <117541067+joaosilva25@users.noreply.github.com>
2026-06-03 09:11:34 -0300

    Update WhatsApp group URL in event constants

--- src/constants/event.ts
+++ src/constants/event.ts
@@ -1,5 +1,5 @@
 export const WHATSAPP_GROUP_URL =
-  'https://chat.whatsapp.com/GgiaeKuxGMB5uSZakNWG2y'
+  'https://chat.whatsapp.com/HqiL10jphxZGZL2FoJeYKn?mode=gi_t'
 
 export const EVENT_GROUP_URL = '/redirect'
 
